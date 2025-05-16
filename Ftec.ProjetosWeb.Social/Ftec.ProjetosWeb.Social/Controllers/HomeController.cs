a1d6fdf5d0d24048479522199c1b6034247bd064
Bruno Madeira <brunolmadeira@gmail.com>
2025-05-16 02:36:45 -0300

    Primeiro commit
    Primeiro protótipo de front

---
+++ Ftec.ProjetosWeb.Social/Ftec.ProjetosWeb.Social/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Ftec.ProjetosWeb.Social.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ftec.ProjetosWeb.Social.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        public IActionResult RedefinirSenha()
+        {
+            return View();
+        }
+
+        public IActionResult Legal()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Ftec.ProjetosWeb.Social/Ftec.ProjetosWeb.Social/Controllers/SocialController.cs
@@ -0,0 +1,57 @@
+﻿using Ftec.ProjetosWeb.Social.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ftec.ProjetosWeb.Social.Controllers
+{
+    public class SocialController : Controller
+    {
+        private readonly ILogger<SocialController> _logger;
+
+        public SocialController(ILogger<SocialController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult Pesquisar()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+
+        public IActionResult Configuracoes()
+        {
+            return View();
+        }
+
+        public IActionResult Publicacao()
+        {
+            return View();
+        }
+
+        public IActionResult Stories()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
